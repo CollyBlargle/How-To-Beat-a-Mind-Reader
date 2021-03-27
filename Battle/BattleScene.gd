e08e1f9f3fd362f268541c5dc6825393c70ecc39
CollyBlargle <collywibbles@gmail.com>
2021-03-26 19:44:45 -0700

    rewrite cutscene

--- Battle/BattleScene.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready():
-	initialize_scene()
-
-func initialize_scene():
-	$CinematicAspectRatio/AnimationPlayer.current_animation = "Squeeze"
-
-#AYYYY WHAT'S GOING ON BROS
--- Music.gd
+++
@@ -1,5 +0,0 @@
-extends AudioStreamPlayer
-
-func _on_DialogueHUD_pastDialogues(dialogueID):
-	if dialogueID == 3:
-		print("dialogueID is 3")
